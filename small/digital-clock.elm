8a329f1fb6127d570be45f5a36e2283da9aa62b8
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-07 14:13:20 -0700

    add pause button

--- small/digital-clock.elm
+++ small/digital-clock.elm
@@ -1,5 +1,6 @@
-import Html exposing (div, text)
+import Html exposing (div, text, button)
 import Html.App exposing (program)
+import Html.Events exposing (onClick)
 import Time exposing (Time, inSeconds, inMinutes, inHours)
 import String
 
@@ -13,29 +14,35 @@ main =
     }
 
 
-type alias Model = Time
+type alias Model =
+  { time : Time
+  , isPaused : Bool
+  }
 
 
 type Msg
   = Tick Time
+  | PauseResume
 
 
 init =
-  (0, Cmd.none)
+  (Model 0 False, Cmd.none)
 
 
 view model =
   let
     seconds =
-      toString <| floor (inSeconds model) `rem` 60
+      toString <| floor (inSeconds model.time) `rem` 60
     minutes =
-      toString <| floor (inMinutes model) `rem` 60
+      toString <| floor (inMinutes model.time) `rem` 60
     hours =
       -- +1 to turn 11 to 12, 0 to 1, ..., -8 for Pacific Standard Time
-      toString <| (floor (inHours model) `rem` 12) + 1 - 8
+      toString <| (floor (inHours model.time) `rem` 12) + 1 - 8
   in
     div []
-      [ text <| hours ++ ":" ++ padStringTime minutes ++ ":" ++ padStringTime seconds ]
+      [ text <| hours ++ ":" ++ padStringTime minutes ++ ":" ++ padStringTime seconds
+      , button [ onClick PauseResume ] [ text (if model.isPaused then "Resume" else "Pause") ]
+      ]
 
 
 padStringTime stringNumber =
@@ -48,7 +55,10 @@ padStringTime stringNumber =
 update msg model =
   case msg of
     Tick newTime ->
-      (newTime, Cmd.none)
+      (if model.isPaused then model else { model | time = newTime }, Cmd.none)
+    
+    PauseResume ->
+      ({ model | isPaused = not model.isPaused }, Cmd.none)
 
 
 subscriptions model =
