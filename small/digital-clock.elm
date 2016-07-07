b0fd17d77f48e3db7a9f38f28c58deeec65ebf21
Bryan Jennings <bryanjenningz@gmail.com>
2016-07-07 13:55:55 -0700

    Create digital-clock.elm

---
+++ small/digital-clock.elm
@@ -0,0 +1,47 @@
+import Html exposing (div, text)
+import Html.App exposing (program)
+import Time exposing (Time, inSeconds, inMinutes, inHours)
+
+
+main =
+  program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+type alias Model = Time
+
+
+type Msg
+  = Tick Time
+
+
+init =
+  (0, Cmd.none)
+
+
+view model =
+  let
+    seconds =
+      toString <| floor (inSeconds model) `rem` 60
+    minutes =
+      toString <| floor (inMinutes model) `rem` 60
+    hours =
+      -- +1 to turn 11 to 12, 0 to 1, ..., -8 for Pacific Standard Time
+      toString <| (floor (inHours model) `rem` 12) + 1 - 8
+  in
+    div []
+      [ text <| hours ++ ":" ++ minutes ++ ":" ++ seconds ]
+
+
+update msg model =
+  case msg of
+    Tick newTime ->
+      (newTime, Cmd.none)
+
+
+subscriptions model =
+  Time.every Time.second Tick
