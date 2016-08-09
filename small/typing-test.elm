cfc46eeb99e433bf14e4bf3a216f9117ba4793a6
Bryan Jennings <bryanjenningz@gmail.com>
2016-08-09 03:48:47 -0700

    Update typing-test.elm

--- small/typing-test.elm
+++ small/typing-test.elm
@@ -1,18 +1,18 @@
-import Html exposing (text, div, input)
+import Html exposing (text, div, input, span)
 import Html.Events exposing (onInput)
-import Html.Attributes exposing (value)
+import Html.Attributes exposing (value, style)
 import Html.App exposing (program)
-import Array exposing (fromList, length, get)
+import Array exposing (fromList, toList, length, get)
 import Maybe exposing (withDefault)
 import Random exposing (generate, int)
-import String exposing (endsWith)
+import String exposing (endsWith, dropRight, split)
 import Time exposing (Time, second)
 
-words = fromList ["apple", "banana", "carrot", "date", "edemame", "fig"]
+sentence = "This is a short sentence to type for practice."
+words = fromList <| split " " sentence
 
 type Msg
-  = NewWordIndex Int
-  | UpdateInputWord String
+  = UpdateInputWord String
   | Tick Time
 
 main =
@@ -24,52 +24,74 @@ main =
     }
 
 type alias Model =
-  { charCount : Int
-  , inputWord : String
-  , word : String
-  , timeLeft : Int
+  { inputWord : String
+  , wordIndex : Int
+  , time : Int
   , runClock : Bool
+  , wpm : Float
   }
 
-init = (Model 0 "" "" 60 True, generate NewWordIndex (int 0 (length words - 1)))
+init = (Model "" 0 0 False 0, Cmd.none)
 
 view model =
   div []
     [ input [ onInput UpdateInputWord, value model.inputWord ] []
-    , div [] [ text model.word ]
-    , div [] [ text (if model.word == model.inputWord then "OK" else "") ]
-    , div [] [ text (toString model.timeLeft) ]
-    , div [] [ text ( if not model.runClock
-                      then (toString <| model.charCount // 5) ++ " WPM"
-                      else "" )
-             ]
+    , span [] [ text <| toString model.wpm ++ " wpm" ]
+    , div [] <| toList <| Array.indexedMap (viewWord model) words
+    , div
+        [ barContainerStyle ]
+        [ div 
+          [ barStyle <|
+              100 * (toFloat model.wordIndex / toFloat (length words)) ]
+          []
+        ]
     ]
+
+viewWord model index word =
+  span
+    [ if index == model.wordIndex then
+        highlightStyle
+      else
+        style []
+    ]
+    [ text (word ++ " ") ]
 
 update msg model =
   case msg of
-    NewWordIndex index->
-      ({ model | word = (withDefault "zucchini" (get index words)) }, Cmd.none)
     UpdateInputWord inputWord ->
-      if endsWith " " inputWord then
+      if endsWith " " inputWord && model.inputWord == dropRight 1 inputWord then
         ( { model | inputWord = ""
-          , charCount =
-              if model.inputWord == model.word then
-                model.charCount + String.length model.word
-              else
-                model.charCount
-          } 
-        , generate NewWordIndex (int 0 (length words))
+          , wordIndex = model.wordIndex + 1
+          , runClock = model.wordIndex < length words
+          , wpm = (toFloat (String.length sentence) / (toFloat model.time / 60)) / 5
+          },
+          Cmd.none
         )
       else
-        ({ model | inputWord = inputWord }, Cmd.none)
+        ( { model | inputWord = inputWord }, Cmd.none )
+        
     Tick time ->
       if model.runClock then
-        ( { model | timeLeft = model.timeLeft - 1
-          , runClock = model.timeLeft > 1
-          }
-        , Cmd.none)
+        ( { model | time = model.time + 1 }, Cmd.none )
       else
-        (model, Cmd.none)
+        ( model, Cmd.none )
 
 subscriptions model =
   Time.every second Tick
+
+highlightStyle =
+  style [ ("background-color", "yellow") ]
+
+barContainerStyle =
+  style
+    [ ("width", "200px")
+    , ("height", "20px")
+    , ("border", "2px solid black")
+    ]
+
+barStyle percentage =
+  style
+    [ ("width", toString percentage ++ "%")
+    , ("height", "20px")
+    , ("background-color", "blue")
+    ]
